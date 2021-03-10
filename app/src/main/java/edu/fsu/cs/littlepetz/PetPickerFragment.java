2095a8b27c98547c810a6cf0557318e3d91b7584
Mina Andrawis <mi.andrawis@gmail.com>
2021-03-10 17:12:12 -0500

    Renamed xml files

--- app/src/main/java/edu/fsu/cs/littlepetz/PetPickerFragment.java
+++ app/src/main/java/edu/fsu/cs/littlepetz/PetPickerFragment.java
@@ -7,9 +7,10 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 public class PetPickerFragment extends Fragment {
+
+
 
     @Override
     public View onCreateView(
@@ -17,11 +18,12 @@ public class PetPickerFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.pet_picker, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
 
 
     }
