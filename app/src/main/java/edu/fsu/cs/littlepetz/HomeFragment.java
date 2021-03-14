14a620497f706ee351c1da2d4e780a38521e81cd
Mina Andrawis <mi.andrawis@gmail.com>
2021-03-13 21:37:28 -0500

    Home Fragment is working
    Only when the bunny option is picked

--- app/src/main/java/edu/fsu/cs/littlepetz/HomeFragment.java
+++ app/src/main/java/edu/fsu/cs/littlepetz/HomeFragment.java
@@ -20,12 +20,12 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.pet_picker, container, false);
+        return inflater.inflate(R.layout.home_fragment, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        
+
     }
 
 }
