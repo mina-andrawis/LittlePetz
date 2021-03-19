f38f4d49523fa946e21e6b5fa97043e257fdc314
PrachiKapur <48018895+PrachiKapur@users.noreply.github.com>
2021-03-18 22:29:17 -0400

    Added a Dialog box to take in the pet name.

--- app/src/main/java/edu/fsu/cs/littlepetz/PetPickerFragment.java
+++ app/src/main/java/edu/fsu/cs/littlepetz/PetPickerFragment.java
@@ -1,12 +1,17 @@
 package edu.fsu.cs.littlepetz;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -18,11 +23,38 @@ import java.util.Objects;
 public class PetPickerFragment extends Fragment {
 
 
+private String name = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.pet_picker, container, false);
+        View v =  inflater.inflate(R.layout.pet_picker, container, false);
+        Button PetName = (Button) v.findViewById(R.id.getPetName);
+        PetName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Please Enter a name: ");
+                final EditText input = new EditText(getContext());
+                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+                builder.setView(input);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        name = input.getText().toString();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+
+            }
+        });
+        return v;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
