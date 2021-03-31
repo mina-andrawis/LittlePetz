c50b0e505f177e6f48bc043ab8ac2e36e80516b5
Mina Andrawis <mi.andrawis@gmail.com>
2021-03-30 22:10:04 -0400

    fixed orientation of home fragment in homeactivity

--- app/src/main/java/edu/fsu/cs/littlepetz/HomeActivity.java
+++ app/src/main/java/edu/fsu/cs/littlepetz/HomeActivity.java
@@ -33,9 +33,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
 
 
         FragmentManager fragmentManager = getFragmentManager();
@@ -45,30 +42,6 @@ public class HomeActivity extends AppCompatActivity {
         fragmentTransaction.commit();
 
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
 
 }
