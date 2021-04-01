8d64f45843462da43bd3d7309d624a3149059a47
Mina Andrawis <maa16s@my.fsu.edu>
2021-03-31 21:15:41 -0400

    added variables to retrieve name and progress bars

--- app/src/main/java/edu/fsu/cs/littlepetz/HomeActivity.java
+++ app/src/main/java/edu/fsu/cs/littlepetz/HomeActivity.java
@@ -3,6 +3,7 @@ package edu.fsu.cs.littlepetz;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -21,25 +22,48 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ViewModel mViewModel;       //ViewModel for petPicker to homeFragment data transmission
-    ImageView petImageView;        // for pet pics
+    TextView petName;
+    ImageView petType;
+    ProgressBar hungerBar, thirstBar, happinessBar;
+
+    public static final String MYPREF = "MyPref" ;
+    public static final String PET_NAME = "petName";
+    public static final String PET_TYPE = "petType";
+
+    public static final String HUNGER_LEVEL = "hungerLevel";
+    public static final String THIRST_LVEL = "thirstLevel";
+    public static final String HAPPINESS_LEVEL = "happinessLevel";
+
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
+        //replace container with homefragment
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, new HomeFragment());
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+
+
+        petName = (TextView) findViewById(R.id.homeName);
+        // petType = ... ?
+        hungerBar = (ProgressBar) findViewById(R.id.hungerBar);
+        thirstBar = (ProgressBar) findViewById(R.id.thirstBar);
+        happinessBar = (ProgressBar) findViewById(R.id.happinessBar);
+
+        
+
 
     }
 
