a14e349887b1f25f3dfe13950dbdd9d9bd76d214
Mina Andrawis <maa16s@my.fsu.edu>
2021-04-09 19:33:52 -0400

    deleted uneeded imports and code

--- app/src/main/java/edu/fsu/cs/littlepetz/HomeActivity.java
+++ app/src/main/java/edu/fsu/cs/littlepetz/HomeActivity.java
@@ -2,30 +2,16 @@ package edu.fsu.cs.littlepetz;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.ViewModel;
-//import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
-import android.view.View;
-
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
@@ -49,7 +35,13 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // initialize prefrence and retrieve UI elements as objects
+        //only called when user first opens app
+        //initialize toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        Log.i("Toolbar", String.valueOf(toolbar));
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+
 
         //replace container with homefragment
         FragmentManager fragmentManager = getFragmentManager();
@@ -59,8 +51,31 @@ public class HomeActivity extends AppCompatActivity {
         fragmentTransaction.commit();
 
 
-
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_friends) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 
 
 }
