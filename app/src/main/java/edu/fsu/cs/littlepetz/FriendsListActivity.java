4a94477751118f69db3b43bba26c9e87b57f67c4
Mina Andrawis <maa16s@my.fsu.edu>
2021-04-11 23:42:51 -0400

    find objects to uselater in FriendsListActivity

--- app/src/main/java/edu/fsu/cs/littlepetz/FriendsListActivity.java
+++ app/src/main/java/edu/fsu/cs/littlepetz/FriendsListActivity.java
@@ -1,25 +1,46 @@
 package edu.fsu.cs.littlepetz;
 
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+// ADAPTED FROM CONTENT PROVIDER CLASS EXAMPLE //
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FriendsListActivity extends AppCompatActivity {
 
+
+    Button addFriendButton;
+
+    EditText addFriendEdit;
+
+    Cursor mCursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         setContentView(R.layout.activity_friends);
         super.onCreate(savedInstanceState);
-    }
 
+
+        addFriendButton = (Button) findViewById(R.id.addFriendButton);
+        addFriendEdit = (EditText) findViewById(R.id.addFriendEditText);
+
+
+    }
 
 
     public void onClickAddFriend(View view)
     {
-        
+
     }
 }
 
